156a60fbe4b07b9e89b47635d0502d9f32e24e92
Manuel Meyer <manuel@vikingosegundo.de>
2012-12-19 15:52:48 +0100

    test of kal from storyboard in tabbarcontroller

---
+++ TabbedKalTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TabbedKalTest
+//
+//  Created by Manuel Meyer on 19.12.12.
+//  Copyright (c) 2012 bit.fritze. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
